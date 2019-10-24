65129add5011b8cb3d655fa7926ff72e7bdc5349
rtsorge <RTSorge@email.uark.edu>
2019-10-23 22:10:45 -0500

    Initial upload

--- app/src/main/java/com/ryan/geo_camera/MainActivity.java
+++ app/src/main/java/com/ryan/geo_camera/MainActivity.java
@@ -3,21 +3,40 @@ package com.ryan.geo_camera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dispatchTakePictureIntent();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
+
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap)extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
