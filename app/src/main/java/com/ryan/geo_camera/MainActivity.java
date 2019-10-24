4993ba0e2bf547b482316f17a0304bfe6b30565f
rtsorge <RTSorge@email.uark.edu>
2019-10-23 20:26:45 -0500

    upload

--- app/src/main/java/com/ryan/geo_camera/MainActivity.java
+++ app/src/main/java/com/ryan/geo_camera/MainActivity.java
@@ -1,17 +1,44 @@
 package com.ryan.geo_camera;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCamera;
+    ImageView imageview;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCamera = (Button)findViewById(R.id.btnCamera);
+        imageview = (ImageView)findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageview.setImageBitmap(bitmap);
     }
 
     @Override
