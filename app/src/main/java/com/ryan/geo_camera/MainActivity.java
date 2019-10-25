289dfc9302e307ac9f83165f75a56a97935e2c12
rtsorge <RTSorge@email.uark.edu>
2019-10-24 20:59:19 -0500

    Starting over

--- app/src/main/java/com/ryan/geo_camera/MainActivity.java
+++ app/src/main/java/com/ryan/geo_camera/MainActivity.java
@@ -1,42 +1,177 @@
 package com.ryan.geo_camera;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.content.FileProvider;
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.DIRECTORY_PICTURES;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    Button cameraButton;
+    ImageView mImageView;
+    String mCurrentPhotoPath;
+    Uri photoURI;
+    GPSTracker gps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dispatchTakePictureIntent();
+        if(Build.VERSION.SDK_INT >= 23)
+            requestPermissions(new String[]{Manifest.permission.CAMERA,
+                                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                            Manifest.permission.ACCESS_FINE_LOCATION,
+                                            Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+
+
+        mImageView = (ImageView)findViewById(R.id.imageView);
+        cameraButton = (Button)findViewById(R.id.btnCamera);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
+    
+    @Override
+    protected void onResume(){
+        super.onResume();
 
     }
 
     private void dispatchTakePictureIntent(){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePictureIntent.resolveActivity(getPackageManager()) != null)
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        {
+            File photoFile = null;
+            photoFile = createImageFile();
+
+            if(photoFile != null)
+            {
+                mCurrentPhotoPath = photoFile.getAbsolutePath();
+                photoURI = FileProvider.getUriForFile(this, "com.ryan.geo_camera.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+            else
+            {
+                Toast.makeText(this, "photoFile = null", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
-        {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap)extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE) {
+            gps = new GPSTracker(MainActivity.this);
+            if (resultCode == RESULT_OK){
+                if(data != null) {
+                    previewImage();
+                    if(gps.canGetLocation())
+                    {
+                        double latitude = gps.getLatitude();
+                        double longitude = gps.getLongitude();
+                        Toast.makeText(this, "Your location is: " + latitude + " " + longitude, Toast.LENGTH_SHORT).show();
+                        Log.d("location", "Latitude: " + latitude);
+                        Log.d("location", "Longitude: " + longitude);
+                        Intent intent = new Intent(this, MapsActivity.class);
+                        intent.putExtra("latitude", latitude);
+                        intent.putExtra("longitude", longitude);
+                        startActivity(intent);
+
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "couldn't get location", Toast.LENGTH_SHORT).show();
+                    }
+                    //previewCapturedImage();
+                }
+            }
+        }
+        else if (resultCode == RESULT_CANCELED) {
+            // user cancelled Image capture
+            Toast.makeText(getApplicationContext(),
+                    "Cancelled", Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            // failed to capture image
+            Toast.makeText(getApplicationContext(),
+                    "Error!", Toast.LENGTH_SHORT)
+                    .show();
         }
     }
+
+    private File createImageFile(){
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        gps = new GPSTracker(MainActivity.this);
+        double latitude = (double)Math.round(gps.getLatitude() * 10000d) / 10000d;
+        double longitude = (double)Math.round(gps.getLongitude() * 10000d) / 10000d;
+        String imageFileName = "JPEG_" + timeStamp + "_" + latitude + "_" + longitude + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        }catch (IOException e) {Log.d("mylog", "Exception: " + e.toString());}
+        return image;
+    }
+
+    private void previewImage()
+    {
+        Uri uri = photoURI;
+        Bitmap bitmap = null;
+        try{
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+        }catch(IOException e) {Log.d("mylog", "Exception: " + e.toString());}
+        Toast.makeText(this, "Image saved to:\n" + uri.toString(), Toast.LENGTH_LONG).show();
+        mImageView.setImageBitmap(bitmap);
+    }
+
+    private void previewCapturedImage() {
+        try {
+            mImageView.setVisibility(View.VISIBLE);
+
+            // bimatp factory
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            // downsizing image as it throws OutOfMemory Exception for larger
+            // images
+            options.inSampleSize = 8;
+
+            final Bitmap bitmap = BitmapFactory.decodeFile(photoURI.getPath(), options);
+
+           mImageView.setImageBitmap(bitmap);
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- app/src/main/java/com/ryan/geo_camera/MapsActivity.java
+++ app/src/main/java/com/ryan/geo_camera/MapsActivity.java
@@ -3,26 +3,42 @@ package com.ryan.geo_camera;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.File;
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
+    ArrayList<String> fileNames = new ArrayList<String>();
+    ArrayList<pair> photoInformation = new ArrayList<pair>();
+    ArrayList<Marker> markers = new ArrayList<Marker>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        File path = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        fileNames = getAllFiles(path.toString());
+        photoInformation = splitFileName(fileNames);
+
+
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -36,12 +52,84 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+    {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        double currentLat = getIntent().getExtras().getDouble("latitude");
+        double currentLon = getIntent().getExtras().getDouble("longitude");
+        LatLng currentPlace = new LatLng(currentLat, currentLon);
+
+        for(int i = 0; i < photoInformation.size(); i++)
+        {
+
+            double lat = photoInformation.get(i).latitude;
+            double lon = photoInformation.get(i).longitude;
+            LatLng place = new LatLng(lat, lon);
+            Marker myMarker = mMap.addMarker(new MarkerOptions().position(place).title("Photo Taken"));
+            myMarker.setTag(photoInformation.get(i));
+            markers.add(myMarker);
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPlace));
+    }
+
+    private ArrayList<String> getAllFiles(String directory)
+    {
+        ArrayList<String> result = new ArrayList<String>(); //ArrayList cause you don't know how many files there is
+        File folder = new File(directory); //This is just to cast to a File type since you pass it as a String
+        File[] filesInFolder = folder.listFiles(); // This returns all the folders and files in your path
+        for (File file : filesInFolder) { //For each of the entries do:
+            //check that it's not a dir
+            if (!file.isDirectory())
+                result.add(file.getName()); //push the filename as a string
+        }
+
+        return result;
+    }
+
+    private ArrayList<pair> splitFileName(ArrayList<String> stringToSplit)
+    {
+        ArrayList<pair> split = new ArrayList<pair>();
+        for(int i = 0; i < stringToSplit.size(); i++)
+        {
+            //File name example
+            //JPEG_20191024_171032_36.089_-94.199_6748836076339466660.jpg
+            String[] temp = stringToSplit.get(i).split("_");
+            String name = temp[0]+temp[1]+temp[2];
+            double lat = Double.valueOf(temp[3]);
+            double lon = Double.valueOf(temp[4]);
+            pair p = new pair(name, lat, lon);
+            split.add(p);
+        }
+        return split;
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker)
+    {
+        pair test = (pair) marker.getTag();
+        if(marker == null);
+        for(int i = 0; i < photoInformation.size(); i++)
+        {
+            if (test.equals(photoInformation.get(i)))
+            {
+                finish();
+            }
+        }
+        return false;
+    }
+}
+
+class pair
+{
+    String photoName;
+    double latitude;
+    double longitude;
+
+    public pair(String photoName, double latitude, double longitude)
+    {
+        this.photoName = photoName;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 }
